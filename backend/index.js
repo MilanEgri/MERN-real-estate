47f081484d65a4b8174f605c59178a914b10790c
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-15 19:04:51 +0100

    create db connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT =8000
+const  {MONGO_URL}  = process.env;
+
+if (!MONGO_URL) {
+    console.error("Missing MONGO_URL environment variable");
+    process.exit(1);
+  }
+
 const app =express()
 
 
-
-
-
-app.listen(PORT,() =>{
-    console.log(`server is running on port ${PORT}`)
-})
+const main = async () => {
+    await mongoose.connect(MONGO_URL);
+  
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`)
+    });
+  };
+  
+  main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
