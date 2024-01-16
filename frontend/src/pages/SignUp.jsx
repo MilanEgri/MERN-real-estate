1cc5ba61e1369dda331333bb6157a526e0161f70
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-16 22:07:26 +0100

    copleet sign up page ui

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='sign-up'>
+      <h1>Sing up</h1>
+      <form>
+        <input type='text' placeholder='Username' id='username' name='username' />
+        <input type='email' placeholder='Email' id="email" name='email' />
+        <input type='password' placeholder='Password' id='password' name='password' />
+        <button className='sign-up-sign-up-button'>Sing Up</button>
+        <button>Continue with google</button>
+      </form>
+      <p>Have an accout? <Link to={"/sign-in"}>Sign in</Link></p>
+    </div>
   )
 }
 
