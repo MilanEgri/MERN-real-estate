c362419252b7c05107c8adee6ebd713b5b2b71f0
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-17 16:51:23 +0100

    start /profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
 
+import React, { useState, useContext,useEffect } from 'react'
+import { Link, redirect, useNavigate } from 'react-router-dom'
+import { Context } from "../App";
 const Profile = () => {
+  const [user, setUser] = useContext(Context);
+  let navigate = useNavigate();
+  useEffect(() =>{
+    const id = localStorage.getItem('id');
+    if(id){
+      navigate('/')
+    }
+  },[])
   return (
     <div>Profile</div>
   )
