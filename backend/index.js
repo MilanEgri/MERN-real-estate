47522f76bd41689fc685fd36d55d869faf7179d6
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-17 14:31:03 +0100

    create sign in backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserModel = require("./models/user.model")
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const PORT = 8000
 const { MONGO_URL } = process.env;
@@ -19,7 +20,27 @@ app.post("/signup", async (req, res) => {
   newUser.password = bcrypt.hashSync(newUser.password, 12)
   try {
     const saved = await UserModel.create(newUser);
-    return res.status(201).json(saved);
+    return res.status(201).json("succes");
+  } catch (err) {
+    return res.status(500).json(err.message)
+  }
+});
+app.post("/signin", async (req, res) => {
+  const user = req.body;
+  try {
+    const saved = await UserModel.findOne({ "username": user.username });
+    if (!saved) {
+      return res.status(401).json("invalid crendentials")
+    }
+    if (bcrypt.compareSync(user.password, saved.password)) {
+      const token = jwt.sign({ id: saved._id},process.env.JWT_SECRET)
+      return res.cookie('token',token,{httponly: true,expires: new  Date(Date.now() + 24* 60 * 60 + 1000)}).status(200).json(saved.username)
+    }
+    else {
+      console.log(user.password)
+      console.log(saved.password)
+      return res.status(401).json("invalid crendentials")
+    }
   } catch (err) {
     return res.status(500).json(err.message)
   }
