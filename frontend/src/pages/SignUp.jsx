d94a8fada37292a0461e0c14bed349802468da70
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-17 10:29:32 +0100

    done with sign up frontend

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [displaySucces,setDispalySucces] = useState("none")
+  const [displayError,setDispalyError] = useState("none")
+  let navigate = useNavigate(); 
+  function clearUseStates() {
+    setEmail("")
+    setPassword("")
+    setUsername("")
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, email, password }),
+    }).then((res) => {
+      if(res.ok){
+        setDispalySucces("inline")
+        clearUseStates()
+        setTimeout(() => {
+          setDispalySucces("none")
+          navigate('/sign-in')
+        }, 2000);
+      }else{
+        setDispalyError("inline")
+        setTimeout(() => {
+          setDispalyError("none")
+        }, 2000);
+      }
+    });
+  }
   return (
     <div className='sign-up'>
       <h1>Sing up</h1>
-      <form>
-        <input type='text' placeholder='Username' id='username' name='username' />
-        <input type='email' placeholder='Email' id="email" name='email' />
-        <input type='password' placeholder='Password' id='password' name='password' />
+      <div className='signup-alert signup-error' style={{ "display": displayError }}>
+        <span>Something went wrong</span>
+      </div>
+      <div className='signup-alert signup-succes' style={{ "display": displaySucces }}>
+        <span>Succesfull registration</span>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Username' id='username' name='username' value={username} onChange={e => setUsername(e.target.value)} />
+        <input type='email' placeholder='Email' id="email" name='email' value={email} onChange={e => setEmail(e.target.value)} />
+        <input type='password' placeholder='Password' id='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
         <button className='sign-up-sign-up-button'>Sing Up</button>
         <button>Continue with google</button>
       </form>
