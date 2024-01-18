dca2e598b5a3ed448ee8edd9f3945a7de3ce2677
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-01-18 13:37:02 +0100

    if you modify local storage its kick you out

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,30 @@
 
-import React, { useState, useContext,useEffect } from 'react'
-import { Link, redirect, useNavigate } from 'react-router-dom'
+import React, {  useContext,useEffect } from 'react'
+import {  useNavigate } from 'react-router-dom'
 import { Context } from "../App";
 const Profile = () => {
   const [user, setUser] = useContext(Context);
+  const deleteCookie = (name) => {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+  };
+  function signOut() {
+    localStorage.removeItem('id');
+    setUser(null)
+    deleteCookie('token');
+  }
   let navigate = useNavigate();
   useEffect(() =>{
     const id = localStorage.getItem('id');
-    if(id){
+    if(!id){
       navigate('/')
     }
+    fetch(`/user/${id}`).then((res) => {
+      res.json()
+      if(!res.ok){
+        signOut()
+        navigate('/')
+      }
+    });
   },[])
   return (
     <div>Profile</div>
