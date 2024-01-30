3b6f365f6b475825409ae53196137ef1f8c98413
Milán Egri <milanegri@Milans-MacBook-Air.local>
2024-01-30 10:38:44 +0100

    edit profile working,but poor css

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const UserModel = require("./models/user.model")
-const ListingModel = require("./models/listing.model")
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const UserModel = require("./models/user.model");
+const ListingModel = require("./models/listing.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const PORT = 8000
+const PORT = 8000;
 const { MONGO_URL } = process.env;
 
 if (!MONGO_URL) {
@@ -13,104 +13,152 @@ if (!MONGO_URL) {
   process.exit(1);
 }
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.get("/listings/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const listing = await ListingModel.findOne({ "_id": id});
+    const listing = await ListingModel.findOne({ _id: id });
     if (!listing) {
-      return res.status(501).json("listing not exist")
+      return res.status(501).json("listing not exist");
     }
     return res.status(200).json(listing);
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
-})
+});
 app.get("/listings", async (req, res) => {
   try {
     const listings = await ListingModel.find({});
     if (!listings) {
-      return res.status(501).json("listings not exist")
+      return res.status(501).json("listings not exist");
     }
     return res.status(200).json(listings);
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
-})
+});
 app.get("/user/:id", async (req, res) => {
   const userId = req.params.id;
   try {
-    const user = await UserModel.findOne({ "_id": userId});
+    const user = await UserModel.findOne({ _id: userId });
     if (!user) {
-      return res.status(404).json("User not ound")
+      return res.status(404).json("User not ound");
     }
     return res.status(200).json(user.username);
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
-})
+});
+app.patch("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const user = await UserModel.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).json("User not found");
+    } else {
+      if (username.length > 2) {
+        const usernameIfExist = await UserModel.findOne({ username: username });
+        if (usernameIfExist) {
+          if (usernameIfExist._id != user._id)
+            return res.status(404).json("error");
+        }
+      }
+      if (email.length > 3) {
+        const emailIfExist = await UserModel.findOne({ email: email });
+        if (emailIfExist) {
+          if (emailIfExist._id != user._id)
+            return res.status(404).json("error");
+        }
+      }
+      if (username.length > 2) {
+        await UserModel.findOneAndUpdate(
+          { _id: user._id },
+          { username: username }
+        );
+      }
+      if (email.length > 3) {
+        await UserModel.findOneAndUpdate({ _id: user._id }, { email: email });
+      }
+      if (password.length > 3) {
+        const newPassword = await bcrypt.hashSync(password, 12);
+        await UserModel.findOneAndUpdate(
+          { _id: user._id },
+          { password: newPassword }
+        );
+      }
+    }
+    return res.status(200).json("succes");
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
+});
 app.delete("/user/:id", async (req, res) => {
   const userId = req.params.id;
   try {
-    const user = await UserModel.findOne({ "_id": userId});
+    const user = await UserModel.findOne({ _id: userId });
     if (!user) {
-      return res.status(404).json("User not found")
-    }
-    else{
-      const listings = await ListingModel.find({ "useRef": user._id});
-      if(!listings){
+      return res.status(404).json("User not found");
+    } else {
+      const listings = await ListingModel.find({ useRef: user._id });
+      if (!listings) {
         await UserModel.findByIdAndDelete(userId);
         return res.status(200).json(user.username);
-      }
-      else{
-        await ListingModel.deleteMany({ "useRef": user._id})
+      } else {
+        await ListingModel.deleteMany({ useRef: user._id });
         await UserModel.findByIdAndDelete(userId);
         return res.status(200).json(user.username);
       }
     }
     return res.status(200).json(user.username);
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
-})
-app.post("/create",async(req,res) =>{
+});
+app.post("/create", async (req, res) => {
   const listing = req.body;
   try {
     const saved = await ListingModel.create(listing);
     return res.status(201).json(saved._id);
   } catch (err) {
-    console.log(err)
-    return res.status(500).json(err.message)
+    console.log(err);
+    return res.status(500).json(err.message);
   }
-})
+});
 app.post("/signup", async (req, res) => {
   const newUser = req.body;
-  newUser.password = bcrypt.hashSync(newUser.password, 12)
+  newUser.password = bcrypt.hashSync(newUser.password, 12);
   try {
     const saved = await UserModel.create(newUser);
     return res.status(201).json("succes");
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
 });
 app.post("/signin", async (req, res) => {
   const user = req.body;
   try {
-    const saved = await UserModel.findOne({ "username": user.username });
+    const saved = await UserModel.findOne({ username: user.username });
     if (!saved) {
-      return res.status(401).json("invalid crendentials")
+      return res.status(401).json("invalid crendentials");
     }
     if (bcrypt.compareSync(user.password, saved.password)) {
-      const token = jwt.sign({ id: saved._id }, process.env.JWT_SECRET)
-      return res.cookie('token', token, { httponly: true, expires: new Date(Date.now() + 24 * 60 * 60 + 1000) })
-        .status(200).json({ 'id': saved._id, })
-    }
-    else {
-      return res.status(401).json("invalid crendentials")
+      const token = jwt.sign({ id: saved._id }, process.env.JWT_SECRET);
+      return res
+        .cookie("token", token, {
+          httponly: true,
+          expires: new Date(Date.now() + 24 * 60 * 60 + 1000),
+        })
+        .status(200)
+        .json({ id: saved._id });
+    } else {
+      return res.status(401).json("invalid crendentials");
     }
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
 });
 
@@ -118,7 +166,7 @@ const main = async () => {
   await mongoose.connect(MONGO_URL);
 
   app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port ${PORT}`);
   });
 };
 
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,8 +6,12 @@ import { IoIosCloseCircle } from "react-icons/io";
 const Profile = () => {
   const [user, setUser] = useContext(Context);
   const [userData, setUserData] = useState(null);
-  const [editMode, setEditMode] = useState(true);
+  const [editMode, setEditMode] = useState(false);
   const [displayError, setDispalyError] = useState("none");
+  const [displaySucces, setDispalySucces] = useState("none");
+  const [username,setUsername] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
   const deleteCookie = (name) => {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
   };
@@ -16,12 +20,48 @@ const Profile = () => {
     setUser(null);
     deleteCookie("token");
   }
+  function clearUseStates(){
+    setUsername("");
+    setEmail("");
+    setPassword("")
+  }
   let navigate = useNavigate();
   function handleDelete() {
     return fetch(`/user/${user}`, { method: "DELETE" }).then((res) => {
       if (res.ok) {
         signOut();
         navigate("/deleted");
+      } else {
+        setDispalyError("inline");
+        setTimeout(() => {
+          setDispalyError("none");
+        }, 2000);
+      }
+    });
+  }
+  function handleUpdate(){
+    const id = localStorage.getItem("id");
+    fetch(`/user/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setDispalySucces("inline");
+          setUserData(username)
+          clearUseStates();
+          setTimeout(() => {
+            setDispalySucces("none");
+            setEditMode(false);
+          }, 2000);
+        });
       } else {
         setDispalyError("inline");
         setTimeout(() => {
@@ -54,10 +94,10 @@ const Profile = () => {
           <IoIosCloseCircle className="profile-edit-close" onClick={e => setEditMode(false)}/>
           <div className="profile-edite-profile-in">
             <h1>Edit Profile</h1>
-            <input placeholder="Username" />
-            <input placeholder="Email" />
-            <input placeholder="Password" />
-            <button className="sign-up-sign-up-button">Send</button>
+            <input placeholder="Username" value={username} onChange={e =>setUsername(e.target.value)}/>
+            <input placeholder="Email" value={email} onChange={e =>setEmail(e.target.value)} />
+            <input placeholder="Password" type="password" value={password} onChange={e =>setPassword(e.target.value)}/>
+            <button className="sign-up-sign-up-button" onClick={handleUpdate}>Send</button>
             <p>If you don't want to change something leave it empty</p>
           </div>
         </div>
@@ -68,11 +108,18 @@ const Profile = () => {
       >
         <span>Something went wrong</span>
       </div>
+      <div
+        className="signup-alert signup-succes"
+        style={{ display: displaySucces }}
+      >
+        <span>Succesfull Update</span>
+      </div>
       <h1>Welcome {userData}</h1>
       <Link to="/create" className="profile-create-button">
         Create listing
       </Link>
       <button onClick={handleDelete}>Delete Profile</button>
+      <button onClick={e => setEditMode(true)}>Edit Profile</button>
     </div>
   );
 };
